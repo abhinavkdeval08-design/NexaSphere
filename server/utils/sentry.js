258e1d7dfb713b112a6953fbf4ba6d7e43f02aab
suhaniiz <suhanishrivastava75@gmail.com>
2026-06-08 20:40:35 +0530

    refactor: migrate legacy sentry integration to v8 api (#1414)
    Signed-off-by: suhaniiz <suhanishrivastava75@gmail.com>

--- server/utils/sentry.js
+++ server/utils/sentry.js
@@ -32,12 +32,6 @@ function initializeSentry(app) {
     dsn: dsn,
     environment: process.env.NODE_ENV || 'development',
     integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Sentry.Integrations.Express({
-        app: true,
-        request: true,
-        serverName: true,
-      }),
       ...(nodeProfilingIntegration ? [nodeProfilingIntegration()] : []),
     ],
     tracesSampleRate: isDevelopment ? 1.0 : 0.1,
@@ -45,14 +39,8 @@ function initializeSentry(app) {
     attachStacktrace: true,
   });
 
-  // The request handler must be the first middleware on the app
-  app.use(Sentry.Handlers.requestHandler());
-
-  // TracingHandler creates a trace for every incoming request
-  app.use(Sentry.Handlers.tracingHandler());
-
   return Sentry;
-}
+};
 
 /**
  * Add Sentry error handler middleware
@@ -60,7 +48,7 @@ function initializeSentry(app) {
  */
 function addSentryErrorHandler(app) {
   // The error handler must be the last middleware on the app
-  app.use(Sentry.Handlers.errorHandler());
+  Sentry.setupExpressErrorHandler(app);
 }
 
 /**
